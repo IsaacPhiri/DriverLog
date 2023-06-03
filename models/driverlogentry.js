9539d7bcdc10c3ba8e18855356dc02efa6b35c07
Isaac Phiri <83553630+IsaacPhiri@users.noreply.github.com>
2023-06-03 13:32:16 +0000

    driver log

--- models/driverlogentry.js
+++ models/driverlogentry.js
@@ -14,18 +14,18 @@ export class DriverLogEntry {
     }
 
     displayLog() {
-        console.log('Driver Log:');
-        console.log(`Date: ${this.date}`);
-        console.log(`Start Time: ${this.startTime}`);
-        console.log(`End Time: ${this.endTime}`);
-        console.log(`Trip Purpose: ${this.tripPurpose}`);
-        console.log(`Origin: ${this.origin}`);
-        console.log(`Destination: ${this.destination}`);
-        console.log(`Mileage: ${this.mileage}`);
-        console.log(`Hours of Service: ${this.hOSHours}`);
-        console.log(`Vehicle Information: ${this.vehicleInfo}`);
-        console.log(`Driver Information: ${this.driverInfo}`);
-        console.log(`Remarks: ${this.remarks}`);
-        console.log(`Signature: ${this.signature}`);
-    }
+      console.log('Driver Log:');
+      console.log(`Date: ${this.date}`);
+      console.log(`Start Time: ${this.trip.startTime}`);
+      console.log(`End Time: ${this.trip.endTime}`);
+      console.log(`Trip Purpose: ${this.trip.purpose}`);
+      console.log(`Origin: ${this.trip.origin}`);
+      console.log(`Destination: ${this.trip.destination}`);
+      console.log(`Mileage: ${this.trip.mileage}`);
+      console.log(`Hours of Service: ${this.hoursOfService.hours}`);
+      console.log(`Vehicle Information: Make: ${this.vehicle.make}, Model: ${this.vehicle.model}, License Plate: ${this.vehicle.licensePlate}, Odometer: ${this.vehicle.odometer}`);
+      console.log(`Driver Information: Name: ${this.driver.name}, License Number: ${this.driver.licenseNumber}`);
+      console.log(`Remarks: ${this.remarks}`);
+      console.log(`Signature: ${this.signature}`);
+  }  
 }
