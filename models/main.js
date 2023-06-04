48f6e4d01917ad592a699ca3a5d2449ad259ea9d
Isaac Phiri <83553630+IsaacPhiri@users.noreply.github.com>
2023-06-04 16:28:59 +0000

    testing nrc field

--- models/main.js
+++ models/main.js
@@ -7,8 +7,7 @@ import { Trip } from './trip.js';
 const trip = new Trip('08:00', '12:00', 'Business', 'Office', 'Client Meeting', '50 miles');
 const hoursOfService = new HoursOfService('4 hours');
 const vehicle = new Vehicle('Toyota', 'Camry', 'ABC123', '10,000 miles');
-const driver = new Driver('John Doe', 'ABC123456'); //Done
+const driver = new Driver('John Doe', 'ABC123456', '123456/78/9'); //Done
 const logEntry = new DriverLogEntry('2023-05-31', trip, hoursOfService, vehicle, driver, 'Meeting went well');
 
-logEntry.addSignature('John Doe');
 logEntry.displayLog();
