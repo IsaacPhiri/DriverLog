07ab9a8224e5c7d03ed16ed2053de87026ab591f
Isaac Phiri <83553630+IsaacPhiri@users.noreply.github.com>
2023-06-06 10:37:14 +0000

    corrected a typo in db.js

--- DriverLogServer/config/db.js
+++ DriverLogServer/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connnectDB = async () => {
+const connectDB = async () => {
 	try {
 		mongoose.set('strictQuery', true);
 		await mongoose.connect(db, {
