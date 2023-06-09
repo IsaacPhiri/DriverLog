b5f22a832339e1c158420215eb65147f4c141b57
Isaac Phiri <83553630+IsaacPhiri@users.noreply.github.com>
2023-06-09 11:49:03 +0000

    Changed driver structure in db to a few inouts

--- driverlogserver/models/Driver.js
+++ driverlogserver/models/Driver.js
@@ -9,69 +9,23 @@ const DriverSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	},
-	logEntries: [
-		{
-			date: {
-				type: String,
-				default: Date.nom
-			},
-			startTime: {
-				type: String,
-				default: Date.nom
-			},
-			endTime: {
-				type: String,
-				default: Date.nom
-			},
-			purpose: {
-				type: String,
-				required: false
-			},
-			origin: {
-				type: String,
-				required: false
-			},
-			destination: {
-				type: String,
-				required: false
-			},
-			mileage: {
-				type: String,
-				required: false
-			},
-			hoursOfService: {
-				type: String,
-				required: false
-			},
-			vehicle: {
-				make: {
-					type: String,
-					required: false
-				},
-				model: {
-					type: String,
-					required: false
-				},
-				licensePlate: {
-					type: String,
-					required: false
-				},
-				odometer: {
-					type: String,
-					required: false
-				}
-			},
-			remarks: {
-				type: String,
-				required: false
-			},
-			signature: {
-				type: String,
-				required: false
-			}
-		},
+	purpose: {
+		type: String,
+		required: false
+	},
+	origin: {
+		type: String,
+		required: false
+	},
+	destination: {
+		type: String,
+		required: false
+	},
+	mileage: {
+		type: String,
+		required: false
+	}
 // Additional log entries...
-		   ]
 });
 
-module.exports = Driver = mongoose.model('driver', DriverSchema);
+module.exports = Driver = mongoose.model('driver', DriverSchema)
