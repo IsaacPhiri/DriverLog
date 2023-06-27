98f43ab5a42d94c6650ed3ffb608ae341e4cfee2
Isaac Phiri <isaacphiri315@gmail.com>
2023-06-27 15:19:53 +0200

    Modified drivers and logentries apis

--- driverlogserver/routes/api/drivers.js
+++ driverlogserver/routes/api/drivers.js
@@ -14,48 +14,75 @@ router.get('/test', (req, res) =>
 );
 
 // @route GET api/drivers
-// @description GET all drivers
+// @description Get all drivers
 // @access Public
-router.get('/', (req, res) => {
-	Driver.find()
-	.then(drivers => res.json(drivers))
-	.catch(err => res.status(404).json({ nodriversfound: 'No Drivers Found' }));
+router.get('/', async (req, res) => {
+  try {
+    const drivers = await Driver.find();
+    res.json(drivers);
+  } catch (error) {
+    res.status(404).json({ error: 'No drivers found' });
+  }
 });
 
 // @route GET api/driver/:id
-// @desciption GET single driver by id
+// @description Get single driver by ID
 // @access Public
-router.get('/:id', (req, res) => {
-	Driver.findById(req.params.id)
-	.then(driver => res.json(driver))
-	.catch(err => res.status(404).json({ nodriverfound: 'No Driver Found' }));
+router.get('/:id', async (req, res) => {
+  try {
+    const driver = await Driver.findById(req.params.id);
+    if (!driver) {
+      return res.status(404).json({ error: 'No driver found' });
+    }
+    res.json(driver);
+  } catch (error) {
+    res.status(404).json({ error: 'No driver found' });
+  }
 });
 
-// @route GET api/drivers
+// @route POST api/drivers
 // @description add/save driver
 // @access Public
 router.post('/', (req, res) => {
-  Driver.create(req.body)
-    .then(driver => res.json({ msg: 'Driver added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this driver' }));
+  const newDriver = new Driver({
+    driverName: req.body.driverName,
+    driverLicenseNumber: req.body.driverLicenseNumber,
+    national_ID: req.body.national_ID
+  });
+
+  newDriver.save()
+    .then(driver => {
+      res.json({ msg: 'Driver added successfully', driver });
+    })
+    .catch(err => {
+      res.status(400).json({ error: 'Unable to add this driver' });
+    });
 });
 
-// @route GET api/driver/:id
-// @desciption Update driver
+// @route PUT api/driver/:id
+// @description Update driver
 // @access Public
 router.put('/:id', (req, res) => {
-	Driver.findByIdAndUpdate(req.params.id, req.body)
-	.then(driver => res.json({ msg: 'Updated successfully' }))
-	.catch(err => res.status(400).json({ error: 'Unable to update the Database' }));
+  Driver.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.json({ msg: 'Updated successfully' });
+    })
+    .catch(err => {
+      res.status(400).json({ error: 'Unable to update the database' });
+    });
 });
 
-// @route GET api/driver/:id
+// @route DELETE api/driver/:id
 // @description Delete driver by id
 // @access Public
 router.delete('/:id', (req, res) => {
-	Driver.findByIdAndRemove(req.params.id, req.body)
-	.then(driver => res.json({ msg: 'Driver entry deleted successfully' }))
-	.catch(err => res.status(404).json({ error: 'No such Driver in Db' }));
+  Driver.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.json({ msg: 'Driver entry deleted successfully' });
+    })
+    .catch(err => {
+      res.status(404).json({ error: 'No such driver in the database' });
+    });
 });
 
 module.exports = router;
--- driverlogserver/routes/api/logEntries.js
+++ driverlogserver/routes/api/logEntries.js
@@ -1,62 +1,89 @@
 // routes/api/logEntries.js
-
 const express = require('express');
 const router = express.Router();
 
-// Load Log model
-const LogEntries = require('../../models/LogEntries');
+// Load LogEntry model
+const LogEntry = require('../../models/LogEntry');
 
-// @route GET api/logentries/test
-// @description tests log route
-// @accesc Public
-
-router.get('/testLog', (req, res) =>
-        res.send('logentry route testing!')
-);
-
-// @route GET api/logEntries
-// @description GET all logentries
+// @route GET api/log-entries
+// @description Get all log entries
 // @access Public
-router.get('/logs', (req, res) => {
-        LogEntries.find()
-        .then(logentries => res.json(logentries))
-        .catch(err => res.status(404).json({ nologentriesfound: 'No LogEntries Found' }));
+router.get('/', (req, res) => {
+  LogEntry.find()
+    .populate('tripId')
+    .then(logEntries => {
+      res.json(logEntries);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Unable to fetch log entries' });
+    });
 });
 
-// @route GET api/logEntries/:id
-// @desciption GET single logEntry by id
+// @route POST api/log-entries
+// @description Create a new log entry
 // @access Public
-router.get('/logs:id', (req, res) => {
-        LogEntries.findById(req.params.id)
-        .then(logEntries => res.json(logEntries))
-        .catch(err => res.status(404).json({ nologentriesfound: 'No Log Entries Found' }));
+router.post('/', (req, res) => {
+  const newLogEntry = new LogEntry({
+    tripId: req.body.tripId,
+    remarks: req.body.remarks
+  });
+
+  newLogEntry.save()
+    .then(logEntry => {
+      res.json(logEntry);
+    })
+    .catch(err => {
+      res.status(400).json({ error: 'Unable to create a new log entry' });
+    });
 });
 
-// @route GET api/logEntries
-// @description add/save logEntry
+// @route GET api/log-entries/:id
+// @description Get a single log entry by ID
 // @access Public
-router.post('/logs', (req, res) => {
-  LogEntries.create(req.body)
-    .then(logEntries => res.json({ msg: 'LogEntry added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this Log entry' }));
+router.get('/:id', (req, res) => {
+  LogEntry.findById(req.params.id)
+    .populate('tripId')
+    .then(logEntry => {
+      if (!logEntry) {
+        return res.status(404).json({ error: 'Log entry not found' });
+      }
+      res.json(logEntry);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Unable to fetch log entry' });
+    });
 });
 
-// @route GET api/logEntries/:id
-// @desciption Update log
+// @route PUT api/log-entries/:id
+// @description Update a log entry by ID
 // @access Public
-router.put('/logs:id', (req, res) => {
-        LogEntries.findByIdAndUpdate(req.params.id, req.body)
-        .then(logEntries => res.json({ msg: 'Updated successfully' }))
-        .catch(err => res.status(400).json({ error: 'Unable to update the Database' }));
+router.put('/:id', (req, res) => {
+  LogEntry.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then(logEntry => {
+      if (!logEntry) {
+        return res.status(404).json({ error: 'Log entry not found' });
+      }
+      res.json(logEntry);
+    })
+    .catch(err => {
+      res.status(400).json({ error: 'Unable to update log entry' });
+    });
 });
 
-// @route GET api/logEntries/:id
-// @description Delete log by id
+// @route DELETE api/log-entries/:id
+// @description Delete a log entry by ID
 // @access Public
-router.delete('/logs:id', (req, res) => {
-        LogEntries.findByIdAndRemove(req.params.id, req.body)
-        .then(logEntries => res.json({ msg: 'LogEntry deleted successfully' }))
-        .catch(err => res.status(404).json({ error: 'No such Log in Db' }));
+router.delete('/:id', (req, res) => {
+  LogEntry.findByIdAndRemove(req.params.id)
+    .then(logEntry => {
+      if (!logEntry) {
+        return res.status(404).json({ error: 'Log entry not found' });
+      }
+      res.json({ msg: 'Log entry deleted successfully' });
+    })
+    .catch(err => {
+      res.status(400).json({ error: 'Unable to delete log entry' });
+    });
 });
 
 module.exports = router;
