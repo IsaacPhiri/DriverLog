97946fb89bc5274d70c131925889a1c33646c3f3
Isaac Phiri <isaacphiri315@gmail.com>
2023-06-27 15:17:37 +0200

    Added new routes to app

---
+++ driverlogserver/app.js
@@ -0,0 +1,40 @@
+// app2.js
+
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+// routes
+const drivers = require('./routes/api/drivers');
+const logentries = require('./routes/api/logEntries');
+const vehicleRoutes = require('./routes/api/vehicle');
+const hoursOfServiceRoutes = require('./routes/api/hoursOfService');
+const trips = require('./routes/api/trip');
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+	res.send('<h1>Welcome to the DriverLog Wep App</h1>')
+);
+
+// use Routes
+app.use('/api/drivers', drivers);
+app.use('/api/logEntries', logentries);
+app.use('/api/vehicle', vehicleRoutes);
+app.use('/api/hours-of-service', hoursOfServiceRoutes);
+app.use('/api/trip', trips);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () =>
+	console.log(`Server running on port ${port}`)
+);
