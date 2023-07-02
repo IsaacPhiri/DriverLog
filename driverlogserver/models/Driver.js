aabda67d2ac900549eba1557518717e02d2de592
Isaac Phiri <isaacphiri315@gmail.com>
2023-07-02 16:11:36 +0200

    Updated driver schema with some input field validations.

--- driverlogserver/models/Driver.js
+++ driverlogserver/models/Driver.js
@@ -9,21 +9,35 @@ const DriverSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	LicenseNumber: {
+	licenseNumber: {
 		type: Number,
 		required: true
 	},
 	nationalId: {
 		type: String,
-		required: true
+		required: true,
+		match: [
+			/^\d{6}\/\d{2}\/\d{1}$/,
+			'National ID should be of the format 123456/12/1',
+		],
 	},
 	contactNumber: {
 		type: Number,
-		required: true
+		required: true,
+		validate: {
+			validator: function (value) {
+			  	return /^\d{10}$/.test(value);
+			},
+			message: 'Contact number must be 10 digits.',
+		},
 	},
 	email: {
 		type: String,
-		required: false
+		required: false,
+		match: [
+			/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+			'Email should be of the format johndoe@gmail.com',
+		  ],
 	},
 	homeAddress: {
 		type: String,
