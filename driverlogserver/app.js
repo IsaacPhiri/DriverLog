712c4dfa8d50286456ea52315f1c1cf4bb56cb1e
Isaac Phiri <isaacphiri315@gmail.com>
2023-07-07 05:02:29 +0200

    Added dotenv library to manage env variables

--- driverlogserver/app.js
+++ driverlogserver/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+require('dotenv').config();
 
 // routes
 const driversRoutes = require('./routes/api/drivers');
