4fd10b8f7f3a926a0827d6d6b104b6bee52bc2dc
Isaac Phiri <isaacphiri315@gmail.com>
2023-07-16 23:32:45 +0200

    Added role to jwt creation

--- driverlogserver/utils/auth.js
+++ driverlogserver/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
-const createJWT = (email, userId, duration) => {
-   const payload = { email, userId, duration };
+const createJWT = (email, userId, duration, role) => {
+   const payload = { email, userId, duration, role };
    return jwt.sign(payload, process.env.TOKEN_SECRET, {
     expiresIn: duration,
    });
