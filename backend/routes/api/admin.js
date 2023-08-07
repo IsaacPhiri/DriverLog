69693f80cba3fd0e81824ca99685b46d4a37e0ac
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-07 14:06:33 +0200

    Added /me route to retrieve user data based on token after login

--- backend/routes/api/admin.js
+++ backend/routes/api/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getAdmin, getAdmins, createAdmin, updateAdmin, deleteAdmin } = require('../../controllers/adminController');
+const protect = require('../../middleware/authMiddleware');
 
 // routes
-router.get('/', getAdmins);
+router.get('/', protect, getAdmins);
 router.post('/create-admin', createAdmin);
-router.route('/:id').get(getAdmin).put(updateAdmin).delete(deleteAdmin);
+router.route('/:id').put(updateAdmin).delete(deleteAdmin);
+router.get('/me', protect, getAdmin);
 
 module.exports = router;
--- backend/routes/api/drivers.js
+++ backend/routes/api/drivers.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getDrivers, getDriver, createDriver, updateDriver, deleteDriver } = require('../../controllers/driverController');
+const protect = require('../../middleware/authMiddleware');
 
-router.get('/', getDrivers);
+router.get('/', protect, getDrivers);
 router.post('/signup-driver', createDriver);
-router.route('/:id').get(getDriver).put(updateDriver).delete(deleteDriver);
-  
+router.route('/:id').put(updateDriver).delete(deleteDriver);
+router.get('/me', protect, getDriver);
+
 module.exports = router;
