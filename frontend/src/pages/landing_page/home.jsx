4d035c24a8ba16462f4c7f976744054f029c7a60
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-07 14:11:46 +0200

    files for new frontend

---
+++ frontend/src/pages/landing_page/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from '@mui/material';
+import NavBar from "../../components/navbar";
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <Typography variant="h3">Driver Log Home</Typography>
+        </>
+    );
+} 
+
+export default Home;
