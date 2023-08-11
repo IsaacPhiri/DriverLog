00a7c51c490847f3468bee1b33894ea788b174cf
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-11 09:30:48 +0200

    Added a welcome message

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>Dashboard</h1>
+      <p><b>Welcome to DriverLog </b> | Efficiently Track and Manage Your Driver Logs - Simplify Compliance, Maximize Efficiency!</p>
+    </div>
   )
 }
 
