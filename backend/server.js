b830ba4538bdc99f46c1614dd774b33ed420aced
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-16 16:17:00 +0200

    Working on frontend loginScreen - failing with errorL: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined.

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use('/api/trips', tripsRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
 	console.log(`Server running on port ${port}`)
