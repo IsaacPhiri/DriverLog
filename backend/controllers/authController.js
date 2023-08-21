50f5469b5b0c5f3a4ff1fc82a1ba69459e4a6874
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-21 15:03:43 +0200

    Adde checks to validate input fields and check if driver exists....

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,35 +7,40 @@ const createJWT = require("../utils/auth");
 
 const signinDriver = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+      throw new Error('Please enter all fields');
+  }
+
   try {
-      const driver = await Driver.findOne({ email: email });
+    const driver = await Driver.findOne({ email: email });
 
-      if (!driver) {
-          throw new Error('Driver not found');
-      }
-
+    if (!driver) {
+        throw new Error('Driver Not Found');
+    } else {
       const isMatch = await bcrypt.compare(password, driver.password);
 
       if (!isMatch) {
-          throw new Error('Incorrect email or password');
+          throw new Error('Incorrect Email or Password');
+      } else {
+        createJWT(res, driver.email, driver._id, driver.role);
+
+        return res.status(200).json({
+          _id: driver._id,
+          firstName: driver.firstName,
+          lastName: driver.lastName,
+          licenseNumber: driver.licenseNumber,
+          nationalId: driver.nationalId,
+          contactNumber: driver.contactNumber,
+          email: driver.email,
+          homeAddress: driver.homeAddress,
+          licenseExpiryDate: driver.licenseExpiryDate,
+          role: driver.role,
+        });
       }
-
-      createJWT(res, driver.email, driver._id, driver.role);
-
-      return res.status(200).json({
-        _id: driver._id,
-        firstName: driver.firstName,
-        lastName: driver.lastName,
-        licenseNumber: driver.licenseNumber,
-        nationalId: driver.nationalId,
-        contactNumber: driver.contactNumber,
-        email: driver.email,
-        homeAddress: driver.homeAddress,
-        licenseExpiryDate: driver.licenseExpiryDate,
-        role: driver.role,
-      });
+    }
   } catch (err) {
-      throw new Error(err.message);
+    throw new Error(err.message);
   }
 });
 
@@ -46,7 +51,7 @@ const logout = asyncHandler(async (req, res) => {
       httpOnly: true,
       expires: new Date(0)
     })
-    res.status(200).json({ success: true, message: 'User Logged out' });
+    res.status(200).json('User Logged out');
   } catch (err) {
     res.status(500);
     throw(new Error('Internal server error'));
